6c7acd90c18f782805fe89fd10f16cc7cd181b8c
Steven Sternberg <ssternberg@microsoft.com>
2020-12-30 15:51:04 -0500

    Added all non-Function code to Main

--- Format-Menu.psm1
+++ Format-Menu.psm1
@@ -110,36 +110,39 @@ Function Format-Menu {
         Write-Host -Foregroundcolor $BorderFg -BackgroundColor $BorderBg $VBC
     }
 
-    [PSCustomObject]$JSON = $InputJSON | ConvertFrom-Json
-    if ( -NOT (Confirm-JSON)) { break }
+    Function Main {
+        [PSCustomObject]$JSON = $InputJSON | ConvertFrom-Json
+        if ( -NOT (Confirm-JSON)) { break }
 
-    #Variables used for "graphical" menu
-    [string]$HBC = [String][Char]9552           #Horizontal Double Bar
-    [string]$VBC = [String][Char]9553           #Veritcal Double Bar
-    [string]$TLBC = [String][Char]9556          #Top Left Corner Double Bar 
-    [string]$TRBC = [String][Char]9559          #Top Right Coner Double Bar
-    [string]$BLBC = [String][Char]9562          #Bottom Left Corner Double Bar
-    [string]$BRBC = [String][Char]9565          #Bottom Right Coner Double Bar
-    [string]$TMBC = [String][Char]9572          #Top Double Bar Middle Single Bar
-    [string]$MMBC = [String][Char]9578          #Middle Double Bar Middle Single Bar
-    [string]$BMBC = [String][Char]9575          #Bottom Double Bar Middle Single Bar
-    [string]$VHLBC = [String][Char]9568
-    [string]$VHRBC = [String][Char]9571
-    [string]$VSBC = [String][Char]9474          #Vertical Single Bar
+        #Variables used for "graphical" menu
+        [string]$HBC = [String][Char]9552           #Horizontal Double Bar
+        [string]$VBC = [String][Char]9553           #Veritcal Double Bar
+        [string]$TLBC = [String][Char]9556          #Top Left Corner Double Bar 
+        [string]$TRBC = [String][Char]9559          #Top Right Coner Double Bar
+        [string]$BLBC = [String][Char]9562          #Bottom Left Corner Double Bar
+        [string]$BRBC = [String][Char]9565          #Bottom Right Coner Double Bar
+        [string]$TMBC = [String][Char]9572          #Top Double Bar Middle Single Bar
+        [string]$MMBC = [String][Char]9578          #Middle Double Bar Middle Single Bar
+        [string]$BMBC = [String][Char]9575          #Bottom Double Bar Middle Single Bar
+        [string]$VHLBC = [String][Char]9568
+        [string]$VHRBC = [String][Char]9571
+        [string]$VSBC = [String][Char]9474          #Vertical Single Bar
 
-    [int]$BorderBg = if ( $JSON.Colors.PSObject.Properties.name -match "BorderBackground" ) { $JSON.Colors.BorderBackground } else { 0 }
-    [int]$BorderFg = if ( $JSON.Colors.PSObject.Properties.name -match "BorderForeground" ) { $JSON.Colors.BorderForeground } else { 6 }
-    [int]$MenuBg = if ( $JSON.Colors.PSObject.Properties.name -match "MenuBackground" ) { $JSON.Colors.MenuBackground } else { 0 }
-    [int]$MenuFg = if ( $JSON.Colors.PSObject.Properties.name -match "MenuForeground" ) { $JSON.Colors.MenuForeground } else { 10 }
+        [int]$BorderBg = if ( $JSON.Colors.PSObject.Properties.name -match "BorderBackground" ) { $JSON.Colors.BorderBackground } else { 0 }
+        [int]$BorderFg = if ( $JSON.Colors.PSObject.Properties.name -match "BorderForeground" ) { $JSON.Colors.BorderForeground } else { 6 }
+        [int]$MenuBg = if ( $JSON.Colors.PSObject.Properties.name -match "MenuBackground" ) { $JSON.Colors.MenuBackground } else { 0 }
+        [int]$MenuFg = if ( $JSON.Colors.PSObject.Properties.name -match "MenuForeground" ) { $JSON.Colors.MenuForeground } else { 10 }
 
-    #Setup spacing based on Menu length and LineWidth
-    [Int]$FrontBuffer = ($JSON.MenuItems.Count).ToString().length + 2
-    [Int]$BackBuffer = $JSON.LineWidth - $FrontBuffer - 3
-    [string]$CharsFront = $HBC * $FrontBuffer
-    [string]$CharsBack = $HBC * $BackBuffer
-    
-    do {
-        $UserInput = Write-Menu
-    } until (($UserInput -ge 0) -AND ($UserInput -le $JSON.MenuItems.Count - 1))
-    return $UserInput
+        #Setup spacing based on Menu length and LineWidth
+        [Int]$FrontBuffer = ($JSON.MenuItems.Count).ToString().length + 2
+        [Int]$BackBuffer = $JSON.LineWidth - $FrontBuffer - 3
+        [string]$CharsFront = $HBC * $FrontBuffer
+        [string]$CharsBack = $HBC * $BackBuffer
+        
+        do {
+            $UserInput = Write-Menu
+        } until (($UserInput -ge 0) -AND ($UserInput -le $JSON.MenuItems.Count - 1))
+        return $UserInput
+    }
+    Main
 }
